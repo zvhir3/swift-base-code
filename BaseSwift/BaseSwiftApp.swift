c8a067e2953319fea1cb92f6cc18a3e852fb57e1
zahiruddinzainal <zahiruddin.developer@gmail.com>
2024-05-14 16:26:21 +0800

    Initial Commit

---
+++ BaseSwift/BaseSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseSwiftApp.swift
+//  BaseSwift
+//
+//  Created by Nightfall Nest on 14/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct BaseSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
